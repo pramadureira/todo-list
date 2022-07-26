d8e49775162200ecd1991187580a7ba5e99d04e1
Pedro <89313201+pramadureira@users.noreply.github.com>
2022-07-26 14:05:47 +0100

    delete button error fixed

--- script.js
+++ script.js
@@ -1,68 +1,92 @@
+var box_list = document.querySelector('section.box-list')
+var box_name = document.getElementById('content')
+
+function create_box(box_name) {
+    var box = document.createElement('div')
+    box.setAttribute("class", "box")
+    
+    var box_menu = document.createElement('div')
+    box_menu.className = "box-menu"
+
+    var buttons = `<button class="edit-box-button">
+                        <img src="images/icon-pen.svg" alt="Add a task box">
+                    </button>
+                    <button class="del-box-button">
+                        <img src="images/icon-close.svg" alt="Add a task box">
+                    </button>`
+
+    box_menu.innerHTML = `<h3>${box_name}</h3>`
+    box_menu.innerHTML += buttons
+    box.appendChild(box_menu)
+
+    return box
+}
+
+function get_data() {
+    return (JSON.parse(localStorage.getItem('todos')) || [])
+}
+
+function update_data(todo) {
+    localStorage.setItem("todos", JSON.stringify(todo))
+}
+
 function show_boxes() {
-    todo = JSON.parse(localStorage.getItem('todos')) || []
+    var todo = get_data()
 
     if (todo.length != 0) {
-        let box_list = document.querySelector('section.box-list')
         box_list.innerHTML = ""
         for (var box_pos in todo) {
-            let box = document.createElement('div')
-            
-            box.setAttribute("class", "box")
-            let box_menu = document.createElement('div')
-            box_menu.className = "box-menu"
-
-            let buttons = `<button class="edit-box-button">
-                                <img src="images/icon-pen.svg" alt="Add a task box">
-                            </button>
-                            <button class="del-box-button">
-                                <img src="images/icon-close.svg" alt="Add a task box">
-                            </button>`
-
-            box_menu.innerHTML = `<h3>${todo[box_pos]}</h3>`
-            box_menu.innerHTML += buttons
-            box.appendChild(box_menu)
+            let box = create_box(todo[box_pos])
             box_list.appendChild(box)
         }
     }
+
+    check()
 }
 
 function add_box() {
-    todo = JSON.parse(localStorage.getItem('todos')) || []
-    let box_name = document.getElementById('content')
+    todo = get_data()
     
     if (box_name.value.length == 0 || box_name.value.length >= 50) {
-        box_name.value = ""
         box_name.focus()
         alert("Insira uma tarefa com pelo menos um símbolo e com menos de 50 símbolos")
     } else {
         todo.push(box_name.value)
-        box_name.value = ""
-        localStorage.setItem("todos", JSON.stringify(todo))
+        update_data(todo)
+
         show_boxes()
     }
+    box_name.value = ""
 }
 
-show_boxes()
-// Creates a new box if the button is pressed
-document.getElementById('add-box-button').onclick = add_box
+function del_box() {
+    todo = get_data()
+    var box_to_del = this.parentNode.parentNode
+    var box_name = box_to_del.querySelector("h3").innerHTML
+    var index = todo.indexOf(box_name)
+    if (index != -1) {
+        todo.splice(index, 1)
+        box_to_del.remove()
 
-// Delete items
-var list_del = document.querySelectorAll('.del-box-button')
-for (var i=0; i<list_del.length; i++) {
-    list_del[i].onclick = function(){
-        todo = JSON.parse(localStorage.getItem('todos'))
-        var box_to_del = this.parentNode.parentNode
-        var box_name = box_to_del.querySelector("h3").innerHTML
-        var index = todo.indexOf(box_name)
-        if (index != -1) {
-            todo.splice(index, 1)
-            
-            box_to_del.remove()
-            localStorage.setItem("todos", JSON.stringify(todo))
-            show_boxes()
-        }
+        update_data(todo)
+    }
+}
+
+function check() {
+    var del_box_button_list = document.querySelectorAll('.del-box-button')
+    for (var i=0; i<del_box_button_list.length; i++) {
+        del_box_button_list[i].addEventListener("click", del_box)
     }
 }
 
 
+show_boxes()
 
+
+// Creates a new box if either the button or "Enter" is pressed
+document.getElementById('add-box-button').onclick = add_box
+document.getElementById('content').addEventListener("keypress", function (event) {
+    if(event.key === "Enter") {
+        add_box()
+    }
+})
